74c0193872359af0d8766f879efdcf99a28d664f
Purgato96 <matheus_purgato@hotmail.com>
2024-03-24 00:41:36 -0300

    Atualizando o projeto nas duas máquinas

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -5,29 +5,31 @@ namespace App\Http\Controllers;
 use App\Models\Site;
 use Illuminate\Http\Request;
 
-class SiteController extends Controller{
+class SiteController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index(){
+    public function index() {
         return view('home');
     }
+
     public function aboutUs() {
         return view('aboutUs');
     }
 
-    public function services(){
+    public function services() {
         return view('services');
     }
 
-    public function cases(){
+    public function cases() {
         return view('cases');
     }
 
     public function blog() {
         return view('blog');
     }
-    public function contact(){
+
+    public function contact() {
         return view('contact');
     }
 
@@ -35,48 +37,42 @@ class SiteController extends Controller{
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Site $site)
-    {
+    public function show(Site $site) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Site $site)
-    {
+    public function edit(Site $site) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Site $site)
-    {
+    public function update(Request $request, Site $site) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Site $site)
-    {
+    public function destroy(Site $site) {
         //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,6 @@
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', [UserController::class, 'index'])->name('web.home');
-Route::get('/contact', [UserController::class, 'contact'])->name('web.contact');
-Route::get('/about-us', [UserController::class, 'aboutUs'])->name('web.about-us');
-Route::get('/blog', [UserController::class, 'blog'])->name('web.blog');
-Route::post('/usuario', [UserController::class, 'store'])->name('web.store');
-
 Route::controller(SiteController::class)->group(function (){
     Route::get('/', 'index')->name('site.home');
     Route::get('/quem-somos', 'aboutUs')->name('site.about-us');
